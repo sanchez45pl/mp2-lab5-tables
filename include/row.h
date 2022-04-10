2ba601daf0d0eca4eed70342037cd65946e7b261
Aleksandr Maslov <sanchez45pl@gmail.com>
2022-04-10 16:29:45 +0300

    add row.h

---
+++ include/row.h
@@ -0,0 +1,12 @@
+#include "polinom.h"
+
+struct Row {
+    unsigned int key;
+    Polinom *polinom;
+    bool is_deleted;
+
+    Row(unsigned int key, Polinom *polinom) :
+            key(key),
+            polinom(polinom),
+            is_deleted(false) {};
+};
