7f45adbc54035654efb68b31e0f9123162fffb05
Aleksandr Maslov <sanchez45pl@gmail.com>
2022-04-10 21:35:17 +0300

    move "find" and "remove" methods to parent class Table

--- include/table.h
+++ include/table.h
@@ -9,23 +9,49 @@ public:
     std::vector<Row> table;
     unsigned int removed_rows_counter;
 
-    Table(): removed_rows_counter(0){};
+    Table() : removed_rows_counter(0) {};
+
+    void remove(unsigned int key) {
+        for (Row &row: table) {
+            if (row.key == key) {
+                row.is_deleted = true;
+                removed_rows_counter++;
+            }
+        }
+        if (removed_rows_counter > table.size() / 2) {
+            for (int i = 0; i < table.size(); i++) {
+                if (table[i].is_deleted) {
+                    table.erase(table.begin() + i);
+                };
+            }
+            removed_rows_counter = 0;
+        }
+    }
+
+    Polinom *find(int key) {
+        for (Row &row: table) {
+            if (row.key == key) return row.polinom;
+        }
+        return nullptr;
+    }
 
     virtual void print() {
         unsigned int max_key = 0;
         for (Row &row: table) {
             if (row.key > max_key) max_key = row.key;
         }
-        unsigned int max_key_length = (int)log10(max_key);
+        unsigned int max_key_length = (int) log10(max_key);
 
         for (Row &row: table) {
-            unsigned int current_key_length = (int)log10(row.key);
-            std::cout << row.key;
-            for (int i = 0; i < max_key_length - current_key_length; i++) {
-                std::cout << " ";
+            if (!row.is_deleted) {
+                unsigned int current_key_length = (int) log10(row.key);
+                std::cout << row.key;
+                for (int i = 0; i < max_key_length - current_key_length; i++) {
+                    std::cout << " ";
+                }
+                std::cout << " | ";
+                row.polinom->print();
             }
-            std::cout << " | ";
-            row.polinom->print();
         }
     }
 };
--- include/tables/unsorted_table.h
+++ include/tables/unsorted_table.h
@@ -9,28 +9,4 @@ public:
     void push_back(Row row) {
         table.push_back(row);
     }
-
-    void remove(unsigned int key) {
-        for (Row &row: table) {
-            if (row.key == key) {
-                row.is_deleted = true;
-                removed_rows_counter++;
-            }
-        }
-        if (removed_rows_counter > table.size() / 2) {
-            for (int i = 0; i < table.size(); i++) {
-                if (table[i].is_deleted) {
-                    table.erase(table.begin() + i);
-                };
-            }
-            removed_rows_counter = 0;
-        }
-    }
-
-    Polinom *find(int key) {
-        for (Row &row: table) {
-            if (row.key == key) return row.polinom;
-        }
-        return nullptr;
-    }
 };
