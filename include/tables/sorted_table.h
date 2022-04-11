4ce005715f3ae4d7d6e6c33d936c097f5f4f489b
Aleksandr Maslov <sanchez45pl@gmail.com>
2022-04-11 13:57:59 +0300

    add sorted_table.h

--- include/tables/sorted_table.h
+++ include/tables/sorted_table.h
@@ -13,4 +13,51 @@ public:
         }
         table.insert(iterator, row);
     }
+
+    void remove(unsigned int key) {
+        unsigned long index = binary_search(key);
+        if (index != -1){
+            table[index].is_deleted = true;
+        }
+        if (removed_rows_counter > table.size() / 2) {
+            for (int i = 0; i < table.size(); i++) {
+                if (table[i].is_deleted) {
+                    table.erase(table.begin() + i);
+                };
+            }
+            removed_rows_counter = 0;
+        }
+
+
+    }
+
+    Polinom *find(unsigned long key) {
+        unsigned long index = binary_search(key);
+        if (index != -1){
+            return  table[index].polinom;
+        } else {
+            return nullptr;
+        }
+    }
+
+private:
+    unsigned long binary_search(unsigned long key) {
+        unsigned long middle;
+        unsigned long left = 0;
+        unsigned long right = table.size() - 1;
+        while (true) {
+            middle = (left + right) / 2;
+
+            if (key < table[middle].key) {
+                right = middle - 1;
+            } else if (key > table[middle].key) {
+                left = middle + 1;
+            } else {
+                return middle;
+            }
+            if (left > right) {
+                return -1;
+            }
+        }
+    }
 };
